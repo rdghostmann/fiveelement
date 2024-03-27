cb53182e233913fe7b928c5f081225dca2688435
Rd <97230275+rdghostmann@users.noreply.github.com>
2024-03-27 16:47:24 +0100

    Up to Date

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
-const withPWA = require('next-pwa')({
-    dest: 'public'
-  })
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true, 
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
+});
+
 const nextConfig = {};
 
 export default withPWA(nextConfig);
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,17 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Sequence Generator",
+  manifest: "/manifest.json",
   description: "Join the number hunting adventure and discover how many pairs you can match in our exciting game.",
   keywords: " generated sequence, number matching game, number checker, interactive table, observation skills, focus and concentration, engaging game, fun game, number hunting, strategy game, memory improvement, cognitive skills, number pairs, matching numbers, table game, number finder, brain game, time passer, addictive game"
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true} className="bg-slate-300">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
       <body className={inter.className}>
         <header className="mx-auto w-full py-2 bg-slate-500 sticky top-0 left-0 z-10 shadow-sm ">
           <h1 className="font-bold text-center text-xl text-white ">Seqence Generator</h1>
